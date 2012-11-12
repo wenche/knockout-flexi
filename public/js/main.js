845604689b06d57a7cb36b0d802ea71599b7df69
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2012-11-12 13:12:08 +0100

    Endret mappestruktur til public for statiske filer (usikker på om god idé)

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+require.config({
+	paths: {
+		knockout: 'libs/knockout-2.2.0',
+		jquery: 'libs/jquery.min'
+	}
+
+});
+
+require([
+	'knockout',
+	'jquery',
+	'config/global'
+], function(ko, $, g){
+  	'use strict';
+
+	
+  
+  	
+});
