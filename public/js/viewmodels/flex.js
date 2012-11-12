c2d9c7d802bf1eb6ab14e89176c2a50e88688f23
Torstein Skulbru <serrghi@gmail.com>
2012-11-12 14:58:19 +0100

    added some model binding

--- public/js/viewmodels/flex.js
+++ public/js/viewmodels/flex.js
@@ -4,12 +4,30 @@ define(['knockout', 'models/TimeEntry'],
 
 		var FlexViewModel = function( registrations ) {
 			var self = this;
+			self.flexDate = ko.observable();
+			self.flexHours = ko.observable();
+			self.flexDesc = ko.observable();
 
 			self.registrations =  ko.observableArray(ko.utils.arrayMap( registrations, function( registration ) {
 				console.log(registration);
-      			return new TimeEntry( registration.date, registration.hours, registration.description);
+      			return new TimeEntry( registration.date, registration.hours, registration.description, registration.spent);
     		}));
 			
+			self.addFlex = function() {
+				console.log("added");
+				self.registrations.push(new TimeEntry({ date: self.flexDate, hours: self.flexHours, description: self.flexDesc, spent: false}));
+				self.flexDate = "";
+				self.flexDesc = "";
+				self.flexHours = "";
+			};
+
+			self.spendFlex = function() {
+				console.log("spent");
+				self.registrations.push(new TimeEntry({ date: self.flexDate, hours: self.flexHours, description: self.flexDesc, spent: true}));
+				self.flexDate = "";
+				self.flexDesc = "";
+				self.flexHours = "";
+			};
 
 		}
 		return FlexViewModel;
