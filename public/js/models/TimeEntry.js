b15e455fbe75aacb824a6affa747749976eadc2c
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2012-11-12 14:25:47 +0100

    Lagt til kode på model, main og viewmodel

--- public/js/models/TimeEntry.js
+++ public/js/models/TimeEntry.js
@@ -2,8 +2,11 @@ define(['knockout'],
 	function(ko){
 		'use strict'
 		//represent a single menu item
-		var TimeEntry = function(){
-			var self = this;
+		var TimeEntry = function(date, hours, description){
+			this.date = ko.observable(date);
+			this.hours = ko.observable(hours);
+			this.description = ko.observable(description);
+
 		} 
 		return TimeEntry;
 	});
